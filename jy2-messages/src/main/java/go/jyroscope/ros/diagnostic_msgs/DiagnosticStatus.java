6e6f7460da8a9d1981ca6c782ad8610e5abb719e
Marcin Wieczorek <mwieczorek@inovatica.com>
2020-03-30 16:49:51 +0200

    equals without header

--- jy2-messages/src/main/java/go/jyroscope/ros/diagnostic_msgs/DiagnosticStatus.java
+++ jy2-messages/src/main/java/go/jyroscope/ros/diagnostic_msgs/DiagnosticStatus.java
@@ -1,6 +1,8 @@
 package go.jyroscope.ros.diagnostic_msgs;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
 
 import com.jyroscope.annotations.Hide;
 import com.jyroscope.annotations.Message;
@@ -23,6 +25,29 @@ public class DiagnosticStatus {
 
 	@Hide
 	public void set(ArrayList<KeyValue> list) {
-		values = list.toArray(new KeyValue[list.size()]);
+		values = list.toArray(new KeyValue[0]);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		DiagnosticStatus that = (DiagnosticStatus) o;
+		return level == that.level &&
+				Objects.equals(name, that.name) &&
+				Objects.equals(message, that.message) &&
+				Objects.equals(hardwareId, that.hardwareId) &&
+				Arrays.equals(values, that.values);
+	}
+
+	@Override
+	public int hashCode() {
+		int result = Objects.hash(level, name, message, hardwareId);
+		result = 31 * result + Arrays.hashCode(values);
+		return result;
 	}
 }
--- jy2-messages/src/main/java/go/jyroscope/ros/std_msgs/Header.java
+++ jy2-messages/src/main/java/go/jyroscope/ros/std_msgs/Header.java
@@ -3,6 +3,7 @@ package go.jyroscope.ros.std_msgs;
 import com.jyroscope.annotations.*;
 
 import java.time.*;
+import java.util.Objects;
 
 @Message("std_msgs/Header")
 public class Header {
@@ -35,5 +36,24 @@ public class Header {
 	public Header(String frameId, double timeInSeconds) {
 		this.frameId = frameId;
 		setSeconds(timeInSeconds);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Header header = (Header) o;
+		return seq == header.seq &&
+				Objects.equals(stamp, header.stamp) &&
+				Objects.equals(frameId, header.frameId);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(seq, stamp, frameId);
 	}
 }
