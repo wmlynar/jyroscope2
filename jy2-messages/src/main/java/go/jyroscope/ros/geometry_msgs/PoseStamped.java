da4e615a3bde11ef34ddbf76fa1740686364f42e
Wojciech Młynarczyk <wmlynarczyk@inovatica.com>
2020-04-05 19:39:41 +0200

    added convenience constructor to pose stamped

--- jy2-messages/src/main/java/go/jyroscope/ros/geometry_msgs/PoseStamped.java
+++ jy2-messages/src/main/java/go/jyroscope/ros/geometry_msgs/PoseStamped.java
@@ -1,5 +1,6 @@
 package go.jyroscope.ros.geometry_msgs;
 
+import com.github.jy2.messages.Pose2d;
 import com.jyroscope.annotations.Message;
 
 import go.jyroscope.ros.std_msgs.Header;
@@ -7,8 +8,17 @@ import go.jyroscope.ros.std_msgs.Header;
 @Message("geometry_msgs/PoseStamped")
 public class PoseStamped {
 
-    public Header header;
-    public Pose pose;
+	public Header header;
+	public Pose pose;
 
-    
+	public PoseStamped() {
+	}
+
+	public PoseStamped(double seconds, String frameId, Pose2d pose2d) {
+		this.header = new Header();
+		this.header.setSeconds(seconds);
+		this.header.frameId = frameId;
+		this.pose = new Pose(pose2d.x, pose2d.y, 0, pose2d.a);
+	}
+
 }
