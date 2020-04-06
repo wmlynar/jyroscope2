076953f332bf250d9d9abe2cce07080bf0aa2848
Marcin Wieczorek <mwieczorek@inovatica.com>
2020-04-06 12:45:01 +0200

    Float64MultiArray

---
+++ jy2-messages/src/main/java/go/jyroscope/ros/std_msgs/Float64MultiArrayMessage.java
@@ -0,0 +1,12 @@
+package go.jyroscope.ros.std_msgs;
+
+import com.jyroscope.annotations.Message;
+import com.jyroscope.annotations.Primitive;
+
+@Message("std_msgs/Float64MultiArray")
+public class Float64MultiArrayMessage {
+
+	public MultiArrayLayoutMessage layout;
+	double[] data;
+
+}
