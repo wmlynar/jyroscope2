16398d696419c6dcbc28d80e25697912e1b31808
Marcin Wieczorek <mwieczorek@inovatica.com>
2023-06-30 14:49:11 +0200

    factory

--- jy2-messages/src/main/java/go/jyroscope/ros/std_msgs/Float64MultiArrayMessage.java
+++ jy2-messages/src/main/java/go/jyroscope/ros/std_msgs/Float64MultiArrayMessage.java
@@ -8,4 +8,13 @@ public class Float64MultiArrayMessage {
 	public MultiArrayLayoutMessage layout;
 	public double[] data;
 
+	public static Float64MultiArrayMessage createArrayWithSize(int size) {
+		Float64MultiArrayMessage reasult = new Float64MultiArrayMessage();
+		reasult.layout = new MultiArrayLayoutMessage();
+		reasult.layout.dim = new MultiArrayDimensionMessage[1];
+		reasult.layout.dim[0] = new MultiArrayDimensionMessage();
+		reasult.layout.dim[0].size = size;
+		reasult.data = new double[size];
+		return reasult;
+	}
 }
