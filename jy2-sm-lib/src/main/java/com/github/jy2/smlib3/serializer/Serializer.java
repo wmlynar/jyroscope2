a3bace8f6e0faca40d2f46c12ed93aa18a4ac2b0
Wojciech Mlynarczyk <wmlynarczyk@inovatica.com>
2023-08-22 14:07:20 +0200

    fixed eating exception

--- jy2-sm-lib/src/main/java/com/github/jy2/smlib3/serializer/Serializer.java
+++ jy2-sm-lib/src/main/java/com/github/jy2/smlib3/serializer/Serializer.java
@@ -58,7 +58,7 @@ public class Serializer {
 						sb.append(value.toString());
 					} catch (Exception e) {
 						log.error("Exception caught when serializing field " + field.getName()
-								+ ", serialized up till now \"" + sb.toString() + "\"");
+								+ ", serialized up till now \"" + sb.toString() + "\"", e);
 						sb.append("-- Exception when serializing: " + e.getClass() + " --");
 					}
 					sb.append("\n");
