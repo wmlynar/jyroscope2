5395b8350f0092eb5ce4a41743dae29aa48230fa
Wojciech Mlynarczyk <wmlynarczyk@inovatica.com>
2023-10-06 17:15:56 +0200

    test if it removes memory allocations

--- jy2-core/src/main/java/com/github/jy2/log/NodeNameManager.java
+++ jy2-core/src/main/java/com/github/jy2/log/NodeNameManager.java
@@ -3,20 +3,26 @@ package com.github.jy2.log;
 import java.util.HashMap;
 
 public class NodeNameManager {
-	
-	private static HashMap<String,String> threadMap = new HashMap<>();
+
+	private static HashMap<String, Holder<String>> threadMap = new HashMap<>();
 	private static int counter = 0;
 
 	public static synchronized void setNodeName(String nodeName) {
 		String threadGroupName = Thread.currentThread().getThreadGroup().getName();
-		threadMap.put(threadGroupName, nodeName);
+		Holder<String> holder = threadMap.get(threadGroupName);
+		if (holder == null) {
+			holder = new Holder<>(threadGroupName);
+			threadMap.put(threadGroupName, holder);
+		}
+		holder.value = nodeName;
 	}
 
 	public static synchronized String getNodeName() {
 		String threadGroupName = Thread.currentThread().getThreadGroup().getName();
-		return threadMap.get(threadGroupName);
+		Holder<String> holder = threadMap.get(threadGroupName);
+		return (holder == null) ? null : holder.value;
 	}
-	
+
 	public static synchronized String getNextThreadGroupName() {
 		return "hz-" + counter++;
 	}
@@ -24,4 +30,13 @@ public class NodeNameManager {
 	public static synchronized void removeThreadGroup() {
 		threadMap.remove(Thread.currentThread().getThreadGroup().getName());
 	}
+
+	public static class Holder<T> {
+		public T value;
+
+		public Holder(T value) {
+			this.value = value;
+		}
+	}
+
 }
