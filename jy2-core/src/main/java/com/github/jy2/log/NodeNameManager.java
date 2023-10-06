9faa9651ec12056fe6f559bfc3d7f2f33a3c5b33
Wojciech Mlynarczyk <wmlynarczyk@inovatica.com>
2023-10-06 14:54:29 +0200

    Revert "updated node name setting in ubermember"
    This reverts commit f7eee3ce3135f800d1cb2700e6e7539a5d184a8c.

--- jy2-core/src/main/java/com/github/jy2/log/NodeNameManager.java
+++ jy2-core/src/main/java/com/github/jy2/log/NodeNameManager.java
@@ -3,21 +3,21 @@ package com.github.jy2.log;
 import java.util.HashMap;
 
 public class NodeNameManager {
-
-	private static HashMap<ThreadGroup, String> threadMap = new HashMap<>();
+	
+	private static HashMap<String,String> threadMap = new HashMap<>();
 	private static int counter = 0;
 
 	public static synchronized void setNodeName(String nodeName) {
-		ThreadGroup threadGroup = Thread.currentThread().getThreadGroup();
-		threadMap.put(threadGroup, nodeName);
+		String threadGroupName = Thread.currentThread().getThreadGroup().getName();
+		threadMap.put(threadGroupName, nodeName);
 	}
 
 	public static synchronized String getNodeName() {
-		ThreadGroup threadGroup = Thread.currentThread().getThreadGroup();
-		return threadMap.get(threadGroup);
+		String threadGroupName = Thread.currentThread().getThreadGroup().getName();
+		return threadMap.get(threadGroupName);
 	}
-
+	
 	public static synchronized String getNextThreadGroupName() {
-		return "jy-" + counter++;
+		return "hz-" + counter++;
 	}
 }
