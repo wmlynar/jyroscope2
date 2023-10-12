9be4a0b4349cbbb23a9516b1fa922293e51b45b7
Wojciech Mlynarczyk <wmlynarczyk@inovatica.com>
2023-10-12 14:45:36 +0200

    new scheduling with fixed delay

---
+++ jy2-core/src/main/java/com/github/jy2/workqueue/RepeaterProcessor3.java
@@ -0,0 +1,109 @@
+package com.github.jy2.workqueue;
+
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+
+public class RepeaterProcessor3 {
+
+	public static final Object TIMEOUT_MARKER = new Object();
+
+	private final long interval;
+	private final ThreadPoolExecutor executor;
+	private final ScheduledExecutorService scheduledExecutor;
+	private volatile ScheduledFuture<?> future;
+	private boolean isProcessing = false;
+	private Runnable command;
+	private Runnable wakeup = () -> wakeup();
+	private boolean rescheduleAndProcessTimeout = false;
+
+	private int counter;
+
+	public RepeaterProcessor3(Supplier<Boolean> callable, int delay, int interval, int count,
+			ThreadPoolExecutor executor, ScheduledExecutorService scheduledExecutor) {
+		this.interval = interval;
+		this.executor = executor;
+		this.scheduledExecutor = scheduledExecutor;
+		this.counter = 0;
+
+		this.command = () -> {
+			while (count == 0 || counter < count) {
+				++counter;
+				boolean result = callable.get();
+				if (!result) {
+					break;
+				}
+				if (interval > 0) {
+					synchronized (RepeaterProcessor3.this) {
+						if (rescheduleAndProcessTimeout) {
+							rescheduleAndProcessTimeout = false;
+							this.future = scheduledExecutor.scheduleWithFixedDelay(wakeup, interval, interval,
+									TimeUnit.MILLISECONDS);
+						} else {
+							// timeout is already scheduled
+							isProcessing = false;
+							return;
+						}
+					}
+				}
+			}
+			synchronized (RepeaterProcessor3.this) {
+				if (future != null) {
+					future.cancel(false);
+				}
+				isProcessing = false;
+				return;
+			}
+		};
+
+		if (delay > 0 && interval > 0) {
+			synchronized (this) {
+				this.future = scheduledExecutor.scheduleWithFixedDelay(wakeup, delay, interval, TimeUnit.MILLISECONDS);
+			}
+		} else {
+			if (interval > 0) {
+				synchronized (this) {
+					this.future = scheduledExecutor.scheduleWithFixedDelay(wakeup, interval, interval,
+							TimeUnit.MILLISECONDS);
+				}
+			}
+			wakeup();
+		}
+	}
+
+	public void wakeup() {
+		synchronized (this) {
+			if (isProcessing) {
+				if (interval > 0) {
+					rescheduleAndProcessTimeout = true;
+					if (future != null) {
+						future.cancel(false);
+					}
+				}
+			} else {
+				startProcessingMessages();
+			}
+		}
+	}
+
+	private void startProcessingMessages() {
+		isProcessing = true;
+		try {
+			executor.execute(command);
+		} catch (RejectedExecutionException e) {
+			isProcessing = false;
+		}
+	}
+
+	public void stop() {
+		synchronized (this) {
+			if (future != null) {
+				future.cancel(false);
+			}
+			rescheduleAndProcessTimeout = false;
+		}
+	}
+}
--- jy2-di/src/main/java/com/github/jy2/di/internal/Repeater.java
+++ jy2-di/src/main/java/com/github/jy2/di/internal/Repeater.java
@@ -3,37 +3,37 @@ package com.github.jy2.di.internal;
 import java.lang.reflect.Method;
 
 import com.github.jy2.di.annotations.Repeat;
-import com.github.jy2.workqueue.MessageProcessor2;
+import com.github.jy2.workqueue.RepeaterProcessor3;
 
 public class Repeater {
-	
+
 	public Object object;
 	public Method method;
 	public Repeat repeat;
 	public Thread thread;
-	public MessageProcessor2 processor;
-	
+	public RepeaterProcessor3 processor;
+
 	/**
 	 * Shutdown = true indicates that repeater should be shut down.
 	 */
 	public boolean shutdown = false;
-	
+
 	public Repeater(Object object, Method method, Repeat parameters) {
 		this.object = object;
 		this.method = method;
 		this.repeat = parameters;
 	}
-	
+
 	/**
 	 * Shuts down the repeater.
 	 */
 	public void shutdown() {
 		shutdown = true;
-		if(thread!=null) {
+		if (thread != null) {
 			thread.interrupt();
-		} else if(processor!=null) {
-			processor.stopTimer();
+		} else if (processor != null) {
+			processor.stop();
 		}
-		
+
 	}
 }
